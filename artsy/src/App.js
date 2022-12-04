7b4920c1c54db2a8192aac88f53305329c431010
Aminat Ahmed <oahmedaminat@gmail.com>
2022-12-04 02:16:42 +0100

    add files

--- artsy/src/App.js
+++ artsy/src/App.js
@@ -1,11 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// import { BrowserRouter, Route, Router } from "react-router-dom";
+import "./App.css";
+// import Auction from "./pages/Auction";
+// import Drop from "./pages/Drop";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+// import Market from "./pages/Market";
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Home />
     </div>
+   
   );
 }
 
